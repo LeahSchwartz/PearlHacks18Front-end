68a196d3d39d7d6b761b69896366b060e25ca3e8
Sabrina Pin <sabrinapin@Sabrinas-MacBook-Pro.local>
2018-02-10 21:15:31 -0500

    added company name and ranking

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ var dataList = [
 }
 ]
 var companyHTML
+let tableData="";
 for (let i = 0; i < dataList.length; i++){
-  companyHTML += '<option value="' + dataList[i].name + '">'
+  companyHTML += '<option value="' + dataList[i].name + '">';
+  tableData+= "<tr> <td>"+dataList[i].name+"</td> <td>"+dataList[i].rank+"</td> </tr>";
+  console.log("<tr> <td>"+dataList[i].name+"</td> <td>"+dataList[i].rank+"</td> </tr>");
 }
+$("#womensListBody").html(tableData);
 window.onload = function(){
   document.getElementById('companyList').innerHTML = companyHTML
+  document.getElementById('womensListBody').innerHTML = tableData
 }
