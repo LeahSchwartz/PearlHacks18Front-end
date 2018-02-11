97d2c7e751d357e8831c8123fcec33897f11a665
Leah Schwartz <leah.schwartz@duke.edu>
2018-02-11 07:04:37 -0500

    made graph disappear and reappear

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ var dataList = [
     name:'Joe',
     rank: 12,
     forbes_rank: 31,
+    equal_opp: 3
   },
   {
     name:'Uber',
     rank: 8,
-    forbes_rank: 1
+    forbes_rank: 1,
+    equal_opp: 5
   },
   {
     name:'Facebook',
     rank: 14,
-    forbes_rank: 9
+    forbes_rank: 9,
+    equal_opp: 10
 }
 ]
 var companyHTML
@@ -29,34 +32,35 @@ window.onload = function(){
   $("#womensList").DataTable( {
     order: [[1, 'desc']]
   });
-  $("#wRankBox").html("<h1><div id='wRank'>"+dataList[0].rank+"/"+dataList.length+"</div><br>Women Ranking</h1>");
-  var chart = c3.generate({
-    bindto:"#criteria",
-    data: {
-        columns: [
-            ['Equal Opportunities', 3.0],
-            ['Females in Top Leadership', 1.6],
-            ['Management Opportunities', 3.9],
-            ['Maternity Leave', 4.3],
-            ['Salary Satisfaction', 2.1]
-
-        ],
-        type: 'bar',
-        colors: {
-        'Equal Opportunities'  : '#202020',
-        'Females in Top Leadership' : '585858',
-        'Management Opportunities' : '#808080',
-        'Maternity Leave' : '#B8B8B8',
-        'Salary Satisfaction' : '#DCDCDC'
-
-        }
-    },
-    axis: {
-      rotated: true
-    }
-
-});
 }
+// $("#wRankBox").html("<h1><div id='wRank'>"+dataList[0].rank+"/"+dataList.length+"</div><br>Women Ranking</h1>");
+//   var chart = c3.generate({
+//     bindto:"#criteria",
+//     data: {
+//         columns: [
+//             ['Equal Opportunities', 3.0],
+//             ['Females in Top Leadership', 1.6],
+//             ['Management Opportunities', 3.9],
+//             ['Maternity Leave', 4.3],
+//             ['Salary Satisfaction', 2.1]
+//
+//         ],
+//         type: 'bar',
+//         colors: {
+//         'Equal Opportunities'  : '#202020',
+//         'Females in Top Leadership' : '585858',
+//         'Management Opportunities' : '#808080',
+//         'Maternity Leave' : '#B8B8B8',
+//         'Salary Satisfaction' : '#DCDCDC'
+//
+//         }
+//     },
+//     axis: {
+//       rotated: true
+//     }
+//
+// });
+// }
 
 function makeCompanyName(){
   var companyName = document.getElementById('searchBar').value
@@ -73,9 +77,39 @@ function makeCompanyName(){
     }
     document.getElementById('rankDiv').innerHTML = "Rank: " + dataListItem.rank
 
+    $("#wRankBox").html("<h1><div id='wRank'>"+dataList[0].rank+"/"+dataList.length+"</div><br>Women Ranking</h1>");
+    var chart = c3.generate({
+      bindto:"#criteria",
+      data: {
+          columns: [
+              ['Equal Opportunities', 3.0],
+              ['Females in Top Leadership', 1.6],
+              ['Management Opportunities', 3.9],
+              ['Maternity Leave', 4.3],
+              ['Salary Satisfaction', 2.1]
+
+          ],
+          type: 'bar',
+          colors: {
+          'Equal Opportunities'  : '#202020',
+          'Females in Top Leadership' : '585858',
+          'Management Opportunities' : '#808080',
+          'Maternity Leave' : '#B8B8B8',
+          'Salary Satisfaction' : '#DCDCDC'
+
+          }
+      },
+      axis: {
+        rotated: true
+      }
+
+  });
+
     document.getElementById('bigDiv').style.display = 'flex'
+    document.getElementById('row').style.display = 'block'
   } else{
       document.getElementById('bigDiv').style.display = 'none'
+      document.getElementById('row').style.display = 'none'
   }
 
 }
