8b338c9089bc35780e40ff35a717c82c240c3ed4
Leah Schwartz <leah.schwartz@duke.edu>
2018-02-11 08:16:09 -0500

    made bar graph functional

--- script.js
+++ script.js
@@ -42,34 +42,6 @@ window.onload = function(){
     order: [[1, 'desc']]
   });
 }
-// $("#wRankBox").html("<h1><div id='wRank'>"+dataList[0].rank+"/"+dataList.length+"</div><br>Women Ranking</h1>");
-//   var chart = c3.generate({
-//     bindto:"#criteria",
-//     data: {
-//         columns: [
-//             ['Equal Opportunities', 3.0],
-//             ['Females in Top Leadership', 1.6],
-//             ['Management Opportunities', 3.9],
-//             ['Maternity Leave', 4.3],
-//             ['Salary Satisfaction', 2.1]
-//
-//         ],
-//         type: 'bar',
-//         colors: {
-//         'Equal Opportunities'  : '#202020',
-//         'Females in Top Leadership' : '585858',
-//         'Management Opportunities' : '#808080',
-//         'Maternity Leave' : '#B8B8B8',
-//         'Salary Satisfaction' : '#DCDCDC'
-//
-//         }
-//     },
-//     axis: {
-//       rotated: true
-//     }
-//
-// });
-// }
 
 function makeCompanyName(){
   var companyName = document.getElementById('searchBar').value
