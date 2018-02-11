d56d172cddae87e46e5fef3915384613644ac6ec
Sabrina Pin <sabrinapin@Sabrinas-MacBook-Pro.local>
2018-02-10 22:43:13 -0500

    bar graph added

--- script.js
+++ script.js
@@ -28,4 +28,23 @@ window.onload = function(){
   document.getElementById('companyList').innerHTML = companyHTML
   document.getElementById('womensListBody').innerHTML = tableData
   $("#womensList").DataTable();
+  var chart = c3.generate({
+    bindto:"#criteria",
+    data: {
+        columns: [
+            ['Equal Opportunities for Males and Females', 3.0],
+            ['Females in Top Leadership', 1.6],
+            ['Management Opportunities', 3.9],
+            ['Maternity Leave', 4.3],
+            ['Salary Satisfaction', 2.1]
+
+        ],
+      
+        type: 'bar'
+    },
+    axis: {
+      rotated: true
+    }
+
+});
 }
